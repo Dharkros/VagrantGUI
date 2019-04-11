539715d5654ebe446f6b7161371a7b5e98c786a1
Santiago Amo Quintero <44247621+Dharkros@users.noreply.github.com>
2019-04-11 23:51:36 +0200

    up, destroy

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 clear
 # Variables
-
 directorio=~/MVV
+dir_actual=`pwd`
 
 # Functiones
 
-function pausa(){
+function Pausa(){
 	echo ""
 	read -p 'Pulse una tecla "Enter" para continuar...'
 	clear
@@ -27,28 +26,39 @@ function check_f(){
 	fi
 }
 
-function crear_mv(){
+function Crear_mv(){
     check_d
-    read -p 'Nombre de la carpeta: ' ubicacion
+    read -p 'Nombre de la carpeta: ' nombre_carpeta
 
-    while [[ -d $directorio/$ubicacion ]]; do
-			clear
-			read -p 'La carpeta ya existe, porfavor elija otro nombre: '  ubicacion	
-	done
-    
-    mkdir $directorio/$ubicacion
-    add_so
-    dir_actual=`pwd`
-    cd $directorio/$ubicacion
-    pwd 
+    while [[ -d $directorio/$nombre_carpeta ]]; do
+    clear
+    read -p 'La carpeta ya existe, porfavor elija otro nombre: ' nombre_carpeta	
+    done
+
+    mkdir $directorio/$nombre_carpeta
+    Add_so
+    cd $directorio/$nombre_carpeta
     echo $set_SO
-    #vagrant init $set_SO 
-    #vagrant up
+    vagrant init $set_SO 
+    
+   
+    read -p 'Nombre de la maquina: ' nombre_maquinas
+    grep -iw "$nombre_maquinas" $directorio/.maquinas
+    
+    if [[ $? -eq 1 ]]; then
+        echo "$nombre_carpeta:$nombre_maquinas" >> $directorio/.maquinas
+    fi
+    vagrant up
+    
+    read -p 'Quieres conectar ahora a la maquina? ' conecssh
+    conecssh=`echo $conecssh | tr [:upper:] [:lower:]`
+   if [[ $conecssh == "y" || $conecssh == "yes" || $conecssh == "s" || $conecssh == "si" ]]; then
+        vagrant ssh
+   fi
     cd $dir_actual
-    pwd
 }
 
-function add_so(){
+function Add_so(){
     echo "1. Ubuntu 18.04 (64/32 bits)"
     echo "2. Ubuntu 16.04 (64/32 bits)"
     echo "3. Ubuntu 14.04 (64/32 bits)"
@@ -56,11 +66,11 @@ function add_so(){
     echo "5. Centos 6"
     echo "6. Windows 10 (64 bits)"
     read -p 'Introduce una opcion: ' opcion
+    while [[ $arquitertura -ne '32' && $arquitertura -ne '64' && $opcion -lt 5 ]]; do
     read -p '64 bits o 32 bits: ' arquitertura
-    while [[ $arquitertura != '32' && $arquitertura != '64' ]]; do
-			clear
-			read -p 'Escriba 64 para usar una de distro de "64 bits" y 32 pra usar una de "32 bits": ' arquitertura	
-	done
+	clear
+	read -p 'Escriba 64 para usar una de distro de "64 bits" y 32 pra usar una de "32 bits": ' arquitertura	
+    done
     case $opcion in
         1 )
             set_SO="ubuntu/bionic$arquitertura";;
@@ -81,40 +91,67 @@ function add_so(){
 
 }
 
+function Rm_mv(){
+   cat $directorio/.maquinas | awk -F":" '{print $2}' | nl
+   read -p 'Seleciones cual borrar: ' select_drop
+   echo "Se va a eliminar $(cat $directorio/.maquinas | sed -n "$select_drop p" | awk -F":" '{print $2}')"
+   read -p '¿Deseas continuar? (y/n) ' resp
+   resp=`echo $resp | tr [:upper:] [:lower:]`
+   if [[ $resp == "y" || $resp == "yes" || $resp == "s" || $resp == "si" ]]; then
+      cd $directorio/$(cat $directorio/.maquinas | sed -n "$select_drop p" | awk -F":" '{print $1}')
+      yes | vagrant destroy
+      cd $dir_actual
+      rm -r $directorio/$(cat $directorio/.maquinas | sed -n "$select_drop p" | awk -F":" '{print $1}')
+      cat $directorio/.maquinas | sed -i "$select_drop d" $directorio/.maquinas
+      sleep 2s
+   fi   
+} 
 
+function Edit_mv(){
+   clear
+}
 
+function Cx_mv(){
+   cat $directorio/.maquinas | awk -F":" '{print $2}' | nl
+   read -p 'Seleciones la maquina a conectar: ' select_ssh
+   
+   cd $directorio/$(cat $directorio/.maquinas | sed -n "$select_ssh p" | awk -F":" '{print $1}')
+   vagrant ssh
+   cd $dir_actual
+}
 
+while [[ true ]]; do
+    echo "1. Crear maquina"
+    echo "2. Borrar maquina"
+    echo "3. Conectar a mv"
+    echo "4. Editar maquina"
+    echo "5. Salir"
+    read -p 'Introduce una opcion: ' opcion
+    case $opcion in
+    1)
+        check_f .maquinas
+        Crear_mv
+        Pausa;;
 
+    2)
+        Rm_mv
+        Pausa;;
 
+    3)
+        Cx_mv
+        clear;;
+    4)
+        Edit_mv
+        Pausa;;
 
+    5)
+        exit
+        clear;;
+    remove)
+         sudo rm -rf ~/MVV/*;;
+    *)
+        ;;
 
+    esac
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo "1. Crear maquina"
-echo "2. Borrar maquina"
-echo "3. Editar maquina"
-read -p 'Introduce una opcion: ' opcion
-case $opcion in
-    1 )
-		crear_mv;;
-	*)
-		;;
-
-esac
+done
